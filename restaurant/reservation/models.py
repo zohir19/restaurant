812fee55d1b55ef55012fa10185141f63750412b
zohir19 <benabbouzohir@gmail.com>
2019-06-12 22:01:40 +0100

    update

--- restaurant/reservation/models.py
+++ restaurant/reservation/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-# Create your models here.c
-
+# Create your models here.
 class Reservation(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
